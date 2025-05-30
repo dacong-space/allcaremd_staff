cf0d76145e343814a05a667b49204d9a65809f78
你的新GitHub用户名 <GitHub>
2025-05-30 16:04:14 -0400

    Deploy optimized website with improved animations and English-only step process

---
+++ assets/LocalHospital-BbSAOF_i.js
@@ -0,0 +1,1 @@
+import{i as c,j as o}from"./index-Btm-31DG.js";const s=c(o.jsx("path",{d:"M19 3H5c-1.1 0-1.99.9-1.99 2L3 19c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2m-1 11h-4v4h-4v-4H6v-4h4V6h4v4h4z"}),"LocalHospital");export{s as M};
